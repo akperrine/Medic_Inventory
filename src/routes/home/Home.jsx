32ae48adfbdde779ea7aa7459116b0e8c7f8835a
Austin Perrine <akperrine@gmail.com>
2023-07-06 14:37:18 -0700

    refactor get and post request to util folder

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,53 +1,48 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import "./Home.css";
+import {
+  addWarehouse,
+  getWarehouses,
+} from "../../utils/warehouseAPI/WarehouseApi";
 import WarehousePreview from "../../components/warehouse-preview/WarehousePreview";
+import "./Home.css";
 
 export default function Home() {
   const [warehouses, setWarehouses] = useState([]);
   const [toggleAddForm, setToggleAddForm] = useState(false);
   const [toggleUpdateForm, setToggleUpdateForm] = useState(false);
   const [locationInput, setLocationInput] = useState("");
-  console.log(locationInput);
 
-  const getWarehouses = async () => {
-    try {
-      const response = await fetch("http://localhost:8080/warehouse");
-      const data = await response.json();
-      setWarehouses(data);
-    } catch (error) {
-      console.error("Error fetching warehouses:", error);
-    }
-  };
-
+  //Set Warehouses
   useEffect(() => {
-    getWarehouses();
+    getWarehouses()
+      .then((jsonData) => setWarehouses(jsonData))
+      .catch((error) => console.log("Error:", error));
   }, []);
 
-  const handleAddWarehouseForm = () => setToggleAddForm(!toggleAddForm);
+  const handleAddFormVisabiltiy = () => setToggleAddForm(!toggleAddForm);
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:8080/warehouse", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ location: locationInput }),
-      });
+      // const response = await fetch("http://localhost:8080/warehouse", {
+      //   method: "POST",
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      //   body: JSON.stringify({ location: locationInput }),
+      // });
 
-      await getWarehouses();
+      await addWarehouse(locationInput);
 
-      if (response.ok) {
-        console.log("Post request successful");
-      } else {
-        console.log("Post request failed");
-      }
+      await getWarehouses()
+        .then((jsonData) => setWarehouses(jsonData))
+        .catch((error) => console.log("Error:", error));
     } catch (error) {
       console.log(error);
     }
+    setLocationInput("");
+    setToggleAddForm(false);
   };
 
   const handleChange = (e) => {
@@ -58,16 +53,18 @@ export default function Home() {
     <div className="home-container">
       <h3>Current Warehouses</h3>
       {toggleAddForm ? (
-        <form className="add-warehouse-form" onSubmit={handleAddWarehouseForm}>
+        <form className="add-warehouse-form" onSubmit={handleFormSubmit}>
           <label>Add a new location</label>
           <input onChange={handleChange} />
           <div className="add-warehouse-form-btn-container">
             <button type="button">Submit</button>
-            <button onClick={handleFormSubmit}>Cancel</button>
+            <button type="button" onClick={handleAddFormVisabiltiy}>
+              Cancel
+            </button>
           </div>
         </form>
       ) : (
-        <button onClick={handleAddWarehouseForm} className="add-warehouse-btn">
+        <button onClick={handleAddFormVisabiltiy} className="add-warehouse-btn">
           Add warehouse {"\u271A"}
         </button>
       )}
---
+++ src/utils/warehouseAPI/WarehouseApi.js
@@ -0,0 +1,19 @@
+export const getWarehouses = async () => {
+  try {
+    const response = await fetch("http://localhost:8080/warehouse");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching warehouses:", error);
+  }
+};
+
+export const addWarehouse = async (location) => {
+  return await fetch("http://localhost:8080/warehouse", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ location: location }),
+  });
+};
