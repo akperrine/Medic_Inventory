ca8f43c73a8077bffec3c1df92c3f3ec0c30e128
Austin Perrine <akperrine@gmail.com>
2023-07-06 14:37:18 -0700

    add buttons for update and delete warehouses

--- src/components/warehouse-preview/WarehousePreview.jsx
+++ src/components/warehouse-preview/WarehousePreview.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { AiTwotoneEdit } from "react-icons/ai";
+import { HiOutlineTrash } from "react-icons/hi";
 import "./WarehousePreview.css";
 
 function WarehousePreview({ warehouse }) {
@@ -8,6 +10,14 @@ function WarehousePreview({ warehouse }) {
       <Link className="preview-link" to={"/location"} state={warehouse}>
         Manage Inventory
       </Link>
+      <div className="preview-btn-container">
+        <button className="preview-change-btn">
+          <AiTwotoneEdit />
+        </button>
+        <button className="preview-change-btn">
+          <HiOutlineTrash />
+        </button>
+      </div>
     </div>
   );
 }
