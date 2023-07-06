b56d7e369cb7849d609ded58a00d63736bd89d9d
Austin Perrine <akperrine@gmail.com>
2023-07-06 14:37:18 -0700

    add basic styles to Add Inventory form

--- src/routes/warehouseInventory/WarehouseInventory.jsx
+++ src/routes/warehouseInventory/WarehouseInventory.jsx
@@ -3,10 +3,12 @@ import { useLocation } from "react-router-dom";
 import { AiTwotoneEdit } from "react-icons/ai";
 import "./WarehouseInventory.css";
 
-const WarehouseLocation = () => {
-  const [toggleAddInventory, setToggleAddInventory] = useState(false);
+const WarehouseInventory = () => {
   const location = useLocation();
+  const [toggleAddInventory, setToggleAddInventory] = useState(false);
   const warehouse = location.state;
+
+  const handleToggleAdd = () => setToggleAddInventory(!toggleAddInventory);
 
   return (
     <div className="inventory-table-container">
@@ -35,12 +37,31 @@ const WarehouseLocation = () => {
           ))}
         </tbody>
       </table>
-      <div className="inv-btn-container">
-        <button className="inv-btn">Add an Item</button>
-        <button className="inv-btn">Delete an Item</button>
-      </div>
+      {toggleAddInventory ? (
+        <form className="inventory-form">
+          <label>Item Name</label>
+          <input />
+          <label>Quantity</label>
+          <input />
+          <label>Maximum Capacity</label>
+          <input />
+          <div className="add-warehouse-form-btn-container">
+            <button type="button">Submit</button>
+            <button type="button" onClick={handleToggleAdd}>
+              Cancel
+            </button>
+          </div>
+        </form>
+      ) : (
+        <div className="inv-btn-container">
+          <button className="inv-btn" onClick={handleToggleAdd}>
+            Add an Item
+          </button>
+          <button className="inv-btn">Delete an Item</button>
+        </div>
+      )}
     </div>
   );
 };
 
-export default WarehouseLocation;
+export default WarehouseInventory;
