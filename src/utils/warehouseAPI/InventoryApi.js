9b00d3655b6a81549035c7c4171e082c7cdf84e7
Austin Perrine <akperrine@gmail.com>
2023-07-06 14:37:18 -0700

    create updates to inventory tables

--- src/utils/warehouseAPI/InventoryApi.js
+++ src/utils/warehouseAPI/InventoryApi.js
@@ -1,10 +1,20 @@
 export const addInvevtory = async (newInvPayload) => {
-  console.log("inReq ", newInvPayload);
   return await fetch("http://localhost:8080/inventory", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(newInvPayload),
+  });
+};
+
+export const updateInvevtory = async (itemId, invPayloadToUpdate) => {
+  console.log("inReq ", invPayloadToUpdate);
+  return await fetch(`http://localhost:8080/inventory/${itemId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(invPayloadToUpdate),
   });
 };
